79db4a5f390b5666b537b0d5cba9aab857f9e6e5
mrenigma <mrenigmabytes@gmail.com>
2019-11-17 22:50:29 -0800

    Initial Commit

--- ML_simpleGL_HelloWorld/inc/ML_simpleGL_HelloWorld.h
+++ ML_simpleGL_HelloWorld/inc/ML_simpleGL_HelloWorld.h
@@ -11,3 +11,48 @@
 // %BANNER_END%
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#pragma region WindowManager selections
+#if USE_GLFW
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#else
+#ifndef EGL_EGLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#endif
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#ifndef GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES
+#endif
+
+#include <GLES3/gl3.h>
+#include <GLES3/gl3ext.h>
+#endif
+#pragma endregion
+
+struct graphics_context_t {
+
+#if USE_GLFW
+	GLFWwindow* window;
+#else
+	EGLDisplay egl_display;
+	EGLContext egl_context;
+#endif
+
+	GLuint framebuffer_id;
+	GLuint vertex_shader_id;
+	GLuint fragment_shader_id;
+	GLuint program_id;
+
+	graphics_context_t();
+	~graphics_context_t();
+
+	void makeCurrent();
+	void swapBuffers();
+	void unmakeCurrent();
+};
--- ML_simpleGL_HelloWorld/src/ML_simpleGL_HelloWorld.cpp
+++ ML_simpleGL_HelloWorld/src/ML_simpleGL_HelloWorld.cpp
@@ -12,6 +12,194 @@
 
 #include "ML_simpleGL_HelloWorld.h"
 
-int main() {
-	printf("Hello World!\n");
+#include <chrono>
+#include <cmath>
+
+#include <ml_graphics.h>
+#include <ml_head_tracking.h>
+#include <ml_perception.h>
+#include <ml_lifecycle.h>
+#include <ml_logging.h>
+
+#if USE_GLFW
+
+graphics_context_t::graphics_context_t() {
+	if (!glfwInit()) {
+		ML_LOG(Error, "glfwInit() failed");
+		exit(1);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+#if ML_OSX
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#endif
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// We open a 1x1 window here -- this is not where the action happens, however;
+	// this program renders to a texture.  This is done merely to make GL happy.
+	window = glfwCreateWindow(1, 1, "rendering test", NULL, NULL);
+	if (!window) {
+		ML_LOG(Error, "glfwCreateWindow() failed");
+		exit(1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		ML_LOG(Error, "gladLoadGLLoader() failed");
+		exit(1);
+	}
+}
+
+void graphics_context_t::makeCurrent() {
+	glfwMakeContextCurrent(window);
+}
+
+void graphics_context_t::unmakeCurrent() {
+}
+
+void graphics_context_t::swapBuffers() {
+	glfwSwapBuffers(window);
+}
+
+graphics_context_t::~graphics_context_t() {
+	glfwDestroyWindow(window);
+	window = nullptr;
+}
+
+#else // !USE_GLFW
+
+graphics_context_t::graphics_context_t() {
+	egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+	EGLint major = 4;
+	EGLint minor = 0;
+	eglInitialize(egl_display, &major, &minor);
+	eglBindAPI(EGL_OPENGL_API);
+
+	EGLint config_attribs[] = {
+	  EGL_RED_SIZE, 5,
+	  EGL_GREEN_SIZE, 6,
+	  EGL_BLUE_SIZE, 5,
+	  EGL_ALPHA_SIZE, 0,
+	  EGL_DEPTH_SIZE, 24,
+	  EGL_STENCIL_SIZE, 8,
+	  EGL_NONE
+	};
+	EGLConfig egl_config = nullptr;
+	EGLint config_size = 0;
+	eglChooseConfig(egl_display, config_attribs, &egl_config, 1, &config_size);
+
+	EGLint context_attribs[] = {
+	  EGL_CONTEXT_MAJOR_VERSION_KHR, 3,
+	  EGL_CONTEXT_MINOR_VERSION_KHR, 0,
+	  EGL_NONE
+	};
+	egl_context = eglCreateContext(egl_display, egl_config, EGL_NO_CONTEXT, context_attribs);
+}
+
+void graphics_context_t::makeCurrent() {
+	eglMakeCurrent(egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, egl_context);
+}
+
+void graphics_context_t::unmakeCurrent() {
+	eglMakeCurrent(NULL, EGL_NO_SURFACE, EGL_NO_SURFACE, NULL);
+}
+
+void graphics_context_t::swapBuffers() {
+	// buffer swapping is implicit on device (MLGraphicsEndFrame)
+}
+
+graphics_context_t::~graphics_context_t() {
+	eglDestroyContext(egl_display, egl_context);
+	eglTerminate(egl_display);
+}
+
+#endif // USE_GLFW
+
+
+
+// Constants
+const char application_name[] = "com.magicleap.helloworld";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main() 
+{
+	if (glfwInit() == GLFW_FALSE)
+	{
+		ML_LOG(Error, "glfwInit() Failure.");
+		exit(1);
+	}
+
+	//	Tells GLFW how to handle the window and with what version of OpenGL to use.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef ML_OSX
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, application_name, NULL, NULL);
+	if (!window)
+	{
+		ML_LOG(Error, "glfwCreateWindow() Failed to create window.");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		ML_LOG(Error, "gladLoadGLLoader() failed");
+		exit(1);
+	}
+	ML_LOG(Info, "Hello World!");
+	auto start = std::chrono::steady_clock::now();
+
+	// Begin painting window
+	while (!glfwWindowShouldClose(window))
+	{
+		auto msRuntime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+		auto factor = labs(msRuntime % 2000 - 1000) / 1000.0;
+
+		processInput(window);
+
+		glClearColor(1.0f - factor, 0.1f, 0.6f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
